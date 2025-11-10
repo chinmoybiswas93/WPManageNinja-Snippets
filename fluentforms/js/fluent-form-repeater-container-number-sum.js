0d87ce7cef0aa2342c586ba8d7782a29b30a4f90
Chinmoy Biswas <61724571+chinmoybiswas93@users.noreply.github.com>
2025-11-10 16:05:41 +0600

    added fluent-form-repeater-num-sum

---
+++ fluentforms/js/fluent-form-repeater-container-number-sum.js
@@ -0,0 +1,46 @@
+;(function ($) {
+  const repeaterRootName = 'repeater_container'
+  const sumFieldName = 'numeric_field_1'
+
+  // Corrected selectors based on your form's HTML structure.
+  const repeaterNumericFieldsSelector = `div[data-root_name="${repeaterRootName}"] input[type="number"]`
+  const repeaterButtonsSelector = `div[data-root_name="${repeaterRootName}"] .js-container-repeat-buttons`
+  const totalFieldSelector = `input[name="${sumFieldName}"]`
+
+  function updateSum () {
+    let currentSum = 0
+
+    const numericFields = $form.find(repeaterNumericFieldsSelector)
+
+    numericFields.each(function () {
+      const value = parseFloat($(this).val())
+      if (!isNaN(value)) {
+        currentSum += value
+      }
+    })
+
+    const totalField = $form.find(totalFieldSelector)
+    totalField.val(currentSum).trigger('change')
+  }
+
+  function initializeEventListeners () {
+    // Unbind any previous listeners to prevent duplicates.
+    $form.off('.ff-repeater-calc')
+
+    $form.on(
+      'input.ff-repeater-calc',
+      repeaterNumericFieldsSelector,
+      function () {
+        updateSum()
+      }
+    )
+
+    $form.on('click.ff-repeater-calc', repeaterButtonsSelector, function () {
+      setTimeout(updateSum, 100)
+    })
+  }
+
+  initializeEventListeners()
+
+  updateSum()
+})(jQuery)
