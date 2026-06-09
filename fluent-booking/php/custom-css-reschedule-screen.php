906253bba3d618394e72ad216faf0dafe66ddad8
Chinmoy Biswas <61724571+chinmoybiswas93@users.noreply.github.com>
2026-06-09 06:12:40 +0600

    modified custom booking css

--- fluent-booking/php/custom-css-reschedule-screen.php
+++ fluent-booking/php/custom-css-reschedule-screen.php
@@ -1,5 +1,5 @@
 <?php
-
+//Reschedule screen custom css
 add_action('fluent_booking/author_landing_head', function () {
     $is_reschedule_page =
         isset($_GET['fluent-booking'], $_GET['type'], $_GET['meeting_hash']) &&
@@ -14,6 +14,30 @@ add_action('fluent_booking/author_landing_head', function () {
         .fcal_booking_form_wrap .fcal_booking_form .fcal_form_item button {
             background: #f80000 !important;
             color: #ffffff !important;
+        }
+    </style>
+    <?php
+});
+
+//Confirmation screen custom css
+add_action('fluent_booking/main_landing', function () {
+    $is_confirmation_page =
+        isset($_GET['fluent-booking'], $_GET['type'], $_GET['meeting_hash']) &&
+        sanitize_text_field(wp_unslash($_GET['fluent-booking'])) === 'booking' &&
+        sanitize_text_field(wp_unslash($_GET['type'])) === 'confirmation';
+
+    if (!$is_confirmation_page) {
+        return;
+    }
+    ?>
+    <style>
+        .confirmation_page a,
+        .confirmation_page .fcal_back_btn a {
+            color: #e11d48;
+        }
+
+        .confirmation_page .fcal_to_calendars .fcal_cal_items a {
+            border-color: #e11d48;
         }
     </style>
     <?php
